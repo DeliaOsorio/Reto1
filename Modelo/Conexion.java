917053a0faae0a1ed52cb9e6b388f02111b9aaac
DeliaOsorio <107774756+DeliaOsorio@users.noreply.github.com>
2022-07-08 22:05:57 -0500

    Add files via upload

---
+++ Modelo/Conexion.java
@@ -0,0 +1,33 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    //1. Crear la instancia de la clase Connection del JAR JDBC
+    Connection connection;
+    
+    //2. Añadimos el constructor de la clase sin argumentos
+
+    public Conexion() {
+        //3. Intentar hacer conexión con la base de datos desde el constructor de la clase
+        try {
+            //4. Buscamos la clase dentro del JAR que coincide con este nombre com.mysql.cj.jdbc.Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //5. Llamamos la instancia connection a la cual le enviamos los parametros 
+            //de conexión con la base de datos (cadena de conexión, usuario, contraseña)
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/reto1_g56_db", "root", "");
+            if(connection != null){
+                System.out.println("Conexión exitosa");
+            }
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println("Conexión fallida");
+        }
+    }
+
+  //6. Creamos una función que retorne la Connection de la clase
+    public Connection getConnection(){
+        return connection;
+    }}
